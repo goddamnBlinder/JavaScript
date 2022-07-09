e742a05212fe45e3b391b752ca3ff39cfe57b54e
James Chinweike <chinweikechukwudi@gmail.com>
2022-07-10 00:43:44 +0100

    js

--- Test.js
+++ Test.js
@@ -1,13 +1,3 @@
-var car = "Keke";
-if(car == "Honda"){
-    alert("I drove my honda to work" );
-}
-else if (car == "Keke"){
-    alert ("I entered KeKe Napepe to work");
-}
-else if (car == "KIA"){
-    alert ("I ordered a KIA uber");
-}
-else {
-    alert("I trekked with my Legediz Benz");
+while (condition) {
+    console.log
 }

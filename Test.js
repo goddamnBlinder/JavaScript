e0b73cf224ee0c614577675ddfb97cf004edd49f
James Chinweike <chinweikechukwudi@gmail.com>
2022-07-10 22:47:27 +0100

    comit

--- Test.js
+++ Test.js
@@ -1,3 +1,14 @@
-while (condition) {
-    console.log
+function battting(player, distance) {
+    if(distance <= 350){
+        console.log(player," Hit the ball");
+    }
+    else if (distance <= 0){
+        console.log(player, "Struck out!!!");
+    }
+    else {
+        console.log(player," hit a home run");
+    }
+    
 }
+
+battting("Steve", 0);

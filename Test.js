bbe5f818151c17c9168ce376a2983a85338b9c2c
James Chinweike <chinweikechukwudi@gmail.com>
2022-07-17 23:46:12 +0100

    😁

--- Test.js
+++ Test.js
@@ -1,10 +1,3 @@
-var food = "meat";
-if (food == "Apple"){
-    alert("He ate apple") 
-}
-else if(food == "meat"){
-   alert("The meat goes well")
-}
-else{
-    alert("He ate something else")
+changestyle(){
+  
 }

bcc07fde27eb886fce636a9421a77078e8414b2e
James Chinweike <chinweikechukwudi@gmail.com>
2022-07-23 00:03:35 +0100

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,5 +1,5 @@
 function changestyle(){
-     var paragraph= document.getElementsByTagName("para1");
+     var paragraph= document.getElementsByTagName("p");
 
      var changeParaText = paragraph[1].style.fontstyle = "italic"; 
 

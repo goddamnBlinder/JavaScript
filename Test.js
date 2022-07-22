cd168bff64509720dd4f9ffe5684e600617d28ff
James Chinweike <chinweikechukwudi@gmail.com>
2022-07-23 00:39:55 +0100

    update

--- Test.js
+++ Test.js
@@ -1,6 +1,9 @@
 function changeStyle(){
      var paragraph= document.getElementsByTagName("p");
 
+     var changeParaText = paragraph[0].style.fontStyle = "italic"; 
      var changeParaText = paragraph[1].style.fontStyle = "italic"; 
+     var changeParaText = paragraph[2].style.fontStyle = "italic"; 
+     var changeParaText = paragraph[3].style.fontStyle = "italic"; 
 
 }

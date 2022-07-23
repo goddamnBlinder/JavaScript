55489f45f93037015e5e1b36742a929e8ea74f16
James Chinweike <chinweikechukwudi@gmail.com>
2022-07-24 00:06:02 +0100

    updated

--- Test.js
+++ Test.js
@@ -1,4 +1,8 @@
 function changeStyle(){
-      
+    var paragragh = document.getElementsByClassName('para');
+    var changeText = paragragh[0].Style.color = "red";
+    var changeatext = paragragh [1].Style.color = "red";
 }
+
+
 

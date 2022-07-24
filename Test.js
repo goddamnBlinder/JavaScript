e4074b8a6d12ed369a20eb5748b2746ad685460d
James Chinweike <chinweikechukwudi@gmail.com>
2022-07-24 17:40:19 +0100

    Commit

--- Test.js
+++ Test.js
@@ -3,4 +3,6 @@ function changeStyle(){
     var firstText = paragragh[0].innerHTML = "Tear drops";
     var secondText = paragragh [1].innerHTML = " from my eyes ";
     var sum = paragragh[2].innerHTML =  firstText + secondText;
+    var empty = paragragh [0].innerHTML = ''
+    var empty = paragragh [1].innerHTML = '';
 }

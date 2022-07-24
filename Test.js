2a3ca704c2e4c7d76fa4f19522c0effa765c3181
James Chinweike <chinweikechukwudi@gmail.com>
2022-07-24 17:27:07 +0100

    lai lai

--- Test.js
+++ Test.js
@@ -1,8 +1,6 @@
 function changeStyle(){
     var paragragh = document.getElementsByClassName('para');
-    var changeText = paragragh[0].Style.color = "red";
-    var changeatext = paragragh [1].Style.color = "red";
+    var firstText = paragragh[0].innerHTML = "Tear drops";
+    var secondText = paragragh [1].innerHTML = " from my eyes ";
+    var sum = paragragh[2].innerHTML =  firstText + secondText;
 }
-
-
-

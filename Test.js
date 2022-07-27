d5fc4113b21a3ef5c5f8b2bdf9b178847931aca3
James Chinweike <chinweikechukwudi@gmail.com>
2022-07-27 22:11:33 +0100

    we back boys

--- Test.js
+++ Test.js
@@ -1,8 +1,6 @@
 function changeStyle(){
-    var Text = document.querySelector(".para")
-    Text.style.color = "orangeRed";
-}
-function ReturnColor(){
-    var Text = document.querySelector(".para")
-    Text.style.color = "black";
+ var elemennt = document.createElement("p");
+ var main = document.querySelector(".main")
+
+ main.appendChild(elemennt);
 }

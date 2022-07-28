4e8823dabcd27f3a26b9ae50e8569e178fea79b9
James Chinweike <chinweikechukwudi@gmail.com>
2022-07-28 22:35:09 +0100

    up

--- Test.js
+++ Test.js
@@ -1,7 +1,13 @@
 function newParagraph(){
 
- var element = document.createElement("p");
+    var H = document.createElement("h1");
+    var main = document.querySelector(".main");
+    main.appendChild(H);
+    var text = document.createTextNode("Kevin Calvin");
+    H.appendChild(text);
 
+
+ var element = document.createElement("p");
 
  var main = document.querySelector(".main");
  console.log(main);
@@ -10,8 +16,16 @@ function newParagraph(){
  main.appendChild(element);
 
  var text = document.createTextNode("We want to pray, close your eyes put your hands together say your prayers");
+ 
 
  element.appendChild(text);
 
 
 } 
+function Remove(){
+    var element = document.createElement("p")
+    var H = document.querySelector(".main");
+
+    removechild("p");
+    console.log(removechild)
+}

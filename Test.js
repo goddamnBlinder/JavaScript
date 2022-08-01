e92dfa6e2eaf77019bfae26e17db04463188f8d2
James Chinweike <chinweikechukwudi@gmail.com>
2022-08-02 00:38:31 +0100

    vanilla

--- Test.js
+++ Test.js
@@ -1,37 +1,8 @@
-function newParagraph(){
-    var elementH = document.createElement("h1");
-    var main = document.querySelector(".main");
-   main.appendChild(elementH);
-    var textH = document.createTextNode("Kevin Calvin");
-    elementH.appendChild(textH);
+function changeText(){
+  var Parent = document.querySelector("main");
+  var child = parent.childNodes[2];
+  childNodes.style.color = "Blue";
 
 
- var element = document.createElement("p");
-// console.log(main);
-main.appendChild(element);
-var text = document.createTextNode("We want to pray, close your eyes put your hands together say your prayers");
- element.appendChild(text);
-
-
- var pAttribute = document.createAttribute("class")
- pAttribute.value = "test";
- element.setAttributeNode(pAttribute);
-
+}
  
- var pAttributi = document.createAttribute("h1")
- pAttributi.value = "test";
- elementH.setAttributeNode(pAttributi);
-
-
-}
-
-
-function removeHeader(){
-var elementH = document.getElementsByTagName("h1")[1];
-var parent = elementH.parentNode;
-parent.removeChild(elementH);
-
-var elementP = document.getElementsByTagName("p")[4];
-
-parent.removeChild(elementP);
-}

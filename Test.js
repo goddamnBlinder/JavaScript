08968f2ebbc9e060fdd9ccb5f50275e15316e862
James Chinweike <chinweikechukwudi@gmail.com>
2022-08-03 23:00:52 +0100

    update

--- Test.js
+++ Test.js
@@ -1,9 +1,10 @@
 function changeText(){
 
+var child = document.querySelector("#p1")
+var parent = child.parentElement;
+  parent.style.color = 'blue';
+  
 
-  var Parent = document.querySelector(".main");
-  var child = parent.childNodes[2];
-  child.style.color = "blue";
 
 
 }

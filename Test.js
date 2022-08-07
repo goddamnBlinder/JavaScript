d837735018fb49e614e4093244a925c0be844bb0
James Chinweike <chinweikechukwudi@gmail.com>
2022-08-07 15:28:28 +0100

    Correct

--- Test.js
+++ Test.js
@@ -3,22 +3,82 @@ function SumbitForm(){
 
 
 }
+
  
 
-document.getElementById("IncreaseBtn").onclick = function (){
-  document.getElementById("countLabel").innerHTML = count;
-  count+=1;
+// document.getElementById("IncreaseBtn").onclick = function (){
+//   count+=1;
+//   document.getElementById("countLabel").innerHTML = count;
+
+// }
+
+
+// document.getElementById("DecreaseBtn").onclick = function(){
+//   count-=1;
+//   document.getElementById("countLabel").innerHTML = count;
+  
+// }
+
+
+
+// document.getElementById("resetBtn").onclick = function (){
+//   count = 0;
+//   document.getElementById("countLabel").innerHTML = count;
+
+// }
+
+
+// let increaseBtn = document.getElementById("IncreaseBtn");
+// let decreaseBtn = document.getElementById("DecreaseBtn");
+// let resetBtn = document.getElementById("resetBtn");
+// let  counter = document.getElementById("countLabel");
+// let count = counter.innerHTML;
+
+
+
+// increaseBtn.addEventListener('click', increaseCount)
+// decreaseBtn.addEventListener('click', decreaseCount)
+// resetBtn.addEventListener('click', resetCount)
+
+// function increaseCount(){
+//   count++;
+//   counter.innerText = count;
+  
+// }
+
+// function decreaseCount(){
+//   count--;
+//   if(count < 0){
+//     count = 0;
+  
+//   }else{
+//     counter.innerText = count;
+//   }
+//   console.log(count);
+// }
+
+
+// function resetCount(){
+//   counter.textContent = 0;
+// }
+
+                                                        //  Radom Time
+
+
+let changeButton = document.getElementById("RandomBtn");
+
+let  dee = document.getElementById("Jagaban");
+let hp = dee.innerText;
+
+
+changeButton.addEventListener("click", change)
+
+
+function change(){
+
+  count = Math.floor(Math.random());
+  console.log(count);
+
 }
 
 
-document.getElementById("DecreaseBtn").onclick = function(){
-  document.getElementById("countLabel").innerHTML = count;
-  count-=1;
-}
-
-
-
-document.getElementById("resetBtn").onclick = function (){
-  document.getElementById("countLabel").innerHTML = count;
-  count = 0;
-}

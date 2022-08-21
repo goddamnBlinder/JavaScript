218d69b09272d91783a1fd1687c3f57ae08c68c7
James Chinweike <chinweikechukwudi@gmail.com>
2022-08-21 23:12:15 +0100

    Update Test.js

--- Test.js
+++ Test.js
@@ -489,10 +489,10 @@ grades = grades.sort(descendingSort);
 
 grades = forEach(print)
 
+function descendingSort(x, y){
+  return y - x;
+}
 
 function print(element){
   console.log(element);
-}
-function descendingSort(x, y){
-  return y - x,
 }
